8b4da9c4d8fca4a18fc6420a3999f56507f8ee39
wsdxl <dxl20@163.com>
2017-08-30 23:18:31 +0800

    添加了alert按钮实现的方法

--- 20170827.js
+++ 20170827.js
@@ -1,7 +1,8 @@
 require('chromedriver')
 const webdriver=require('selenium-webdriver')
+let until=webdriver.until;
 const driver=new webdriver.Builder().forBrowser('chrome').build()
-driver.get('http://192.168.0.107:3000/')
+driver.get('http://192.168.75.130:3000/')
 driver.manage().window().maximize()
 driver.findElement({linkText:'登录'}).click();
 driver.findElement({id:'name'}).sendKeys('abcduxiaolei');
@@ -12,11 +13,26 @@ driver.findElement({id:'create_topic_btn'}).click(); // 发布话题
 driver.findElement({id:'tab-value'}).click();
 driver.findElement({css:'#tab-value>option:nth-child(2)'}).click() //选择板块nth-child(i)
 // driver.findElement({xpath:'//*[@id="tab-value"]/option[2]'}).click();
-driver.findElement({id:'title'}).sendKeys('大家一起学习');
+driver.findElement({id:'title'}).sendKeys('很晚了，亲们');
 driver.findElement({css:'.eicon-image'}).click();
 driver.findElement({name:'file'}).sendKeys('E://01.png');
-driver.sleep(4000);
-driver.findElement({css:'.CodeMirror-scroll'}).click();// 进入div大块
+driver.sleep(3000);
+driver.findElement({css:'.CodeMirror.cm-s-paper'}).click();// 进入div大块
 let exec=driver.findElement({xpath:'//*[@id="create_topic_form"]/fieldset/div/div/div[2]/div[6]/div[2]'});// div语句块  
-driver.actions().mouseMove(exec).sendKeys('大家晚上好').perform(); //actions方式
+driver.actions().mouseMove(exec).sendKeys('来张帅照，呵呵').perform(); //actions方式
 driver.findElement({className:'span-primary submit_btn'}).submit();
+driver.sleep(3000);
+driver.findElement({css:'.eicon-image'}).click();
+driver.findElement({name:'file'}).sendKeys('F://图片//02.png');
+driver.sleep(3000)
+driver.findElement({css:'.CodeMirror.cm-s-paper'}).click();
+let ox=driver.findElement({xpath:'//*[@id="reply_form"]/div/div/div[2]/div[6]/div[2]'});
+driver.sleep(5000);
+driver.actions().mouseMove(ox).sendKeys('小伙子长得还不错吧?').perform();
+driver.findElement({xpath:'//*[@id="reply_form"]/div/div/div[3]/input'}).submit();
+driver.findElement({css:'.delete_reply_btn'}).click();
+driver.switchTo().alert().then((alert)=>{
+    driver.sleep(5000);
+    return alert.accept();
+})
+
---
+++ 20170828.js
@@ -0,0 +1,22 @@
+require('chromedriver')
+const webdriver=require('selenium-webdriver')
+let until=webdriver.until;
+const driver=new webdriver.Builder().forBrowser('chrome').build()
+driver.get('http://192.168.75.130:3000/')
+driver.manage().window().maximize()
+driver.findElement({linkText:'登录'}).click();
+driver.findElement({id:'name'}).sendKeys('abcduxiaolei');
+driver.findElement({id:'pass'}).sendKeys('abc4862556');
+driver.findElement({css:'.span-primary'}).click();  //登录流程
+driver.findElement({id:'create_topic_btn'}).click(); // 发布话题
+driver.findElement({id:'tab-value'}).click();
+driver.findElement({css:'#tab-value>option:nth-child(2)'}).click() //选择板块nth-child(i)
+// driver.findElement({xpath:'//*[@id="tab-value"]/option[2]'}).click();
+driver.findElement({id:'title'}).sendKeys('今天好像有雨');
+driver.findElement({css:'.eicon-image'}).click();
+driver.findElement({name:'file'}).sendKeys('E://01.png');
+driver.sleep(4000);
+driver.findElement({css:'.CodeMirror-scroll'}).click();// 进入div大块
+let exec=driver.findElement({xpath:'//*[@id="create_topic_form"]/fieldset/div/div/div[2]/div[6]/div[2]'});// div语句块  
+driver.actions().mouseMove(exec).sendKeys('大家注意天气哈').perform(); //actions方式
+driver.findElement({className:'span-primary submit_btn'}).submit();
